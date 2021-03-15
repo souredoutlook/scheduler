f821e736e0b1bd5d149ac1b27160b16d4c149073
souredoutlook <souredoutlook@gmail.com>
2021-03-15 15:43:50 -0400

    add confirm case

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,10 @@ import React from "react";
 import "components/Button.scss";
 
 export default function Button(props) {
+  console.log(props);
   return (
   <>
-    <button>{props.children}</button>
+    <button className={props.confirm && 'button--confirm'}>{props.children}</button>
   </>
   );
 }
