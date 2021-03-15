0e039540abc29a807dd2bec522c87100c1a58c36
souredoutlook <souredoutlook@gmail.com>
2021-03-15 15:52:33 -0400

    add danger case

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,7 +6,7 @@ export default function Button(props) {
   console.log(props);
   return (
   <>
-    <button className={props.confirm && 'button--confirm'}>{props.children}</button>
+    <button className={(props.confirm && 'button--confirm') || (props.danger && 'button--danger')}>{props.children}</button>
   </>
   );
 }
