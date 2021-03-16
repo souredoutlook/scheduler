88beabb7e2b181136d484817155231bd03310d2d
souredoutlook <souredoutlook@gmail.com>
2021-03-16 01:16:35 -0400

    clean up DayList.js

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -18,32 +18,3 @@ export default function DayList(props) {
     </ul>
   );
 }
-
-// const days = [
-//   {
-//     id: 1,
-//     name: "Monday",
-//     spots: 2,
-//   },
-//   {
-//     id: 2,
-//     name: "Tuesday",
-//     spots: 5,
-//   },
-//   {
-//     id: 3,
-//     name: "Wednesday",
-//     spots: 0,
-//   },
-// ];
-
-// storiesOf("DayList", module)
-//   .addParameters({
-//     backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
-//   })
-//   .add("Monday", () => (
-//     <DayList days={days} day={"Monday"} setDay={action("setDay")} />
-//   ))
-//   .add("Tuesday", () => (
-//     <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
-//   ));
