c0233ff297a8cfd96d6ba798c701e998008dbee0
souredoutlook <souredoutlook@gmail.com>
2021-03-16 01:21:41 -0400

    set up interviewList and stories

--- src/components/InterviewerList.js
+++ src/components/InterviewerList.js
@@ -1,20 +1,53 @@
 import React from 'react';
 
-import DayListItem from 'components/DayListItem';
+import InterviewerListItem from 'components/InterviewerListItem';
 
-export default function DayList(props) {
+import 'components/InterviewerList.scss';
 
-  const dayListItems = props.days.map(
-    day => <DayListItem 
-      key = {day.id}
-      name = {day.name}
-      spots = {day.spots}
-      selected = {day.name === props.day}
-      setDay = {props.setDay} />);
+export default function InterviewerList(props) {
+
+  // const dayListItems = props.days.map(
+  //   day => <DayListItem 
+  //     key = {day.id}
+  //     name = {day.name}
+  //     spots = {day.spots}
+  //     selected = {day.name === props.day}
+  //     setDay = {props.setDay} />);
 
   return (
-    <ul>
-      {dayListItems}
-    </ul>
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">
+
+      </ul>
+    </section>
+
   );
 }
+
+// const interviewers = [
+//   { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+//   { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+//   { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+//   { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+//   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+// ];
+    
+// storiesOf("InterviewerList", module)
+//   .addParameters({
+//     backgrounds: [{ name: "dark", value: "#222f3e", default: true }]
+//   })
+//   .add("Initial", () => (
+//     <InterviewerList
+//       interviewers={interviewers}
+//       setInterviewer={action("setInterviewer")}
+//     />
+//   ))
+//   .add("Preselected", () => (
+//     <InterviewerList
+//       interviewers={interviewers}
+//       interviewer={3}
+//       setInterviewer={action("setInterviewer")}
+//     />
+//   ));
+    
