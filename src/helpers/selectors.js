839b1e5206216a1178cb3d7cb494b4e0158efe1a
souredoutlook <souredoutlook@gmail.com>
2021-03-17 16:23:14 -0400

    implement getAppointmentForDay logic

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,5 +1,13 @@
 //selectors.js
 
 export default function getAppointmentsForDay(state, day) {
-  // const filteredAppointments = state.appointments.filter(appointment => appointment.id === ) 
+  const filteredAppointments = [];
+  state.days.forEach(dayElement => {
+    if (dayElement.name === day) {
+      dayElement.appointments.forEach(appointment => {
+        if (state.appointments[appointment]) filteredAppointments.push(state.appointments[appointment])
+      })
+    }  
+  })
+  return filteredAppointments;
 }
