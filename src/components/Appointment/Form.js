8709b78c6235313fdb47cc314d346cf362de37ee
souredoutlook <souredoutlook@gmail.com>
2021-03-16 23:57:40 -0400

    refactor Form component to use props

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,14 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 
 import InterviewerList from 'components/InterviewerList';
 import Button from 'components/Button';
 
 export default function Form(props) {
-  const interviewer = props.interviewer
   const setInterviewer = function() {
 
   }
-  
+  const [name, setName]=useState(props.name || "");
+  const [value, onChange]=useState(props.value || null);
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
@@ -18,21 +19,20 @@ export default function Form(props) {
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            /*
-          This must be a controlled component
-        */
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          value={interviewer}
-          onChange={setInterviewer}
+          value={value}
+          onChange={onChange}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger>Cancel</Button>
-          <Button confirm>Save</Button>
+          <Button danger onClick={props.onCancel}>Cancel</Button>
+          <Button confirm onClick={props.onSave}>Save</Button>
         </section>
       </section>
     </main>
