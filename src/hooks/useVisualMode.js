58d8bf7afa58ffb1b7f0bab93365c8476b2c32fa
souredoutlook <souredoutlook@gmail.com>
2021-03-17 23:54:50 -0400

    create and test useVisualMode

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  
+  const transition = function(newMode) {
+    setMode(newMode);
+  }
+  return { 
+    mode,
+    transition
+  };
+}
