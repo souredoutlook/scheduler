3c534bd16197f8d5e241602088ab01329f73d53a
souredoutlook <souredoutlook@gmail.com>
2021-03-18 00:03:00 -0400

    add transition and backk to useVisualMode

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -2,12 +2,23 @@ import React, { useState } from 'react';
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
   
   const transition = function(newMode) {
     setMode(newMode);
-  }
+    setHistory([newMode, ...history]);
+  };
+
+  const back = function() {
+    if (history.length > 1) {
+      history.shift();
+      setMode(history[0])
+    }
+  };
+
   return { 
     mode,
-    transition
+    transition,
+    back
   };
 }
